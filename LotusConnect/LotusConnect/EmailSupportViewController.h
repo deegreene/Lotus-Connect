6f3083e0c2af11921b0a4047dbf3cedf4e4775ea
Dee Greene <realdeegreene@gmail.com>
2015-10-15 09:44:45 -0400

    Added view controller to email support
    User can click icon from messages tab and be taken to a view controller
    to email support

---
+++ LotusConnect/LotusConnect/EmailSupportViewController.h
@@ -0,0 +1,13 @@
+//
+//  EmailSupportViewController.h
+//  LotusConnect
+//
+//  Created by Dee Greene on 10/15/15.
+//  Copyright © 2015 Dee Greene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EmailSupportViewController : UIViewController
+
+@end
---
+++ LotusConnect/LotusConnect/EmailSupportViewController.m
@@ -0,0 +1,37 @@
+//
+//  EmailSupportViewController.m
+//  LotusConnect
+//
+//  Created by Dee Greene on 10/15/15.
+//  Copyright © 2015 Dee Greene. All rights reserved.
+//
+
+#import "EmailSupportViewController.h"
+
+@interface EmailSupportViewController ()
+
+@end
+
+@implementation EmailSupportViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
